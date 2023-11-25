efdf783c2d8bb186858847a6ec6e3f01cd21c2b3
kashif <engrkashif576@gmail.com>
2023-11-25 22:33:20 +0500

    first commit

---
+++ src/app/youtube/page.tsx
@@ -0,0 +1,13 @@
+function Youtube () {
+return(
+
+    <div>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU?si=QxLeQUULaGkf-2jS" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+
+    </div>
+
+)
+
+
+} export default Youtube
